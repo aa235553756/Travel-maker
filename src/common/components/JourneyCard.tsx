8d209925f6ca557428cbb85fc0d03a6b9e970243
Clara <smile629639@gmail.com>
2023-03-15 02:29:58 +0800

    [fix]修正MoreJourney模組為ts版本

--- src/common/components/JourneyCard.tsx
+++ src/common/components/JourneyCard.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { BsHeart } from 'react-icons/bs'
 
-export default function JourneyCard({ title, number, favorites  }) {
+interface JourneyCardProps {
+  title: string
+  number: number
+  favorites: number
+}
+
+const JourneyCard: React.FC<JourneyCardProps> = ({
+  title,
+  number,
+  favorites,
+}) => {
   return (
     <div className="bg-[#d7d7d7] rounded-lg w-full">
       <div className="flex flex-col space-y-2 w-full">
         <div className="w-full h-[240px] bg-[#ccc] rounded-t-lg">這是圖片</div>
         <div className="flex space-x-2">
-          <div className="w-1/2 bg-[#ccc] h-[120px]">
-            這是圖片
-          </div>
-          <div className="w-1/2 bg-[#ccc] h-[120px]">
-            這是圖片
-          </div>
+          <div className="w-1/2 bg-[#ccc] h-[120px]">這是圖片</div>
+          <div className="w-1/2 bg-[#ccc] h-[120px]">這是圖片</div>
         </div>
       </div>
       <div className="px-5 py-3">
@@ -28,3 +34,5 @@ export default function JourneyCard({ title, number, favorites  }) {
     </div>
   )
 }
+
+export default JourneyCard
--- src/modules/JourneyPage/MoreJourney/index.tsx
+++ src/modules/JourneyPage/MoreJourney/index.tsx
@@ -7,9 +7,9 @@ export default function MoreJourney() {
       <div className="container">
         <h2 className="text-2xl font-bold mb-8">更多隨心所欲行程</h2>
         <div className="flex flex-col space-y-5 mb-[100px] md:flex-row md:space-y-0 md:space-x-6 md:mb-[160px]">
-          <JourneyCard title="好瘋狂熱血少年" number="4" favorite="100" />
-          <JourneyCard title="好瘋狂熱血少年" number="4" favorite="100" />
-          <JourneyCard title="好瘋狂熱血少年" number="4" favorite="100" />
+          <JourneyCard title="好瘋狂熱血少年" number={4} favorites={100} />
+          <JourneyCard title="好瘋狂熱血少年" number={4} favorites={100} />
+          <JourneyCard title="好瘋狂熱血少年" number={4} favorites={100} />
         </div>
       </div>
     </div>
