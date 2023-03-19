d976a2ac9c9f5eda2b08abf56fb1d7279467ab9b
ChiaYang <aa235553756@gmail.com>
2023-03-19 15:13:59 +0800

    [feat]新增react-select

---
+++ src/common/components/searchButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsSearch } from 'react-icons/bs'
+export default function SearchButton({}) {
+  return (
+    <button className="w-full rounded-md py-[10px] bg-[#B9B8B8] md:p-5 md:w-auto">
+      <BsSearch className="inline mx-auto text-xl" />
+    </button>
+  )
+}
+
+// todo
+// 頁籤
+// 搜尋bar
+// select
