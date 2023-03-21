e795f37fae1e7ce6a9c9705aec48ff061c56d629
ChiaYang <aa235553756@gmail.com>
2023-03-22 07:11:40 +0800

    feat/新增SelectSide模組

---
+++ src/modules/JourneyPage/SelectSide.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SelectSide() {
+  return (
+    <div className="container">
+      <form className="mr-6 w-[264px] h-[788px] bg-[#D9D9D9]">
+        <div className="bg-#C4C4C4">選擇行程（必選）</div>
+      </form>
+    </div>
+  )
+}
