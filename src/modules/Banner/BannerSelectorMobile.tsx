dd130e39f0ba02b88a6da3cf64e5266573101adf
ChiaYang <aa235553756@gmail.com>
2023-04-10 00:27:55 +0800

    [fix]修復手機版Safari-100vh問題(2)

--- src/modules/Banner/BannerSelectorMobile.tsx
+++ src/modules/Banner/BannerSelectorMobile.tsx
@@ -27,128 +27,133 @@ export default function BannerSelectorMobile({
 }: BannerSelectorMobileProp) {
   return (
     <>
-      <Div100vh className="fixed z-10">
-        <form
-          className={
-            isHidden
-              ? 'hidden'
-              : 'fixed px-10 py-8 top-0 right-0 w-screen h-screen z-[10]'
-          }
-        >
-          {isHidden ? null : (
-            <div
-              className="absolute bg-[rgba(0,0,0,0.2)] px-10 py-8 top-0 right-0  w-screen h-screen !overflow-y-scroll z-[-1]"
-              onClick={() => {
+      <form className={isHidden ? 'hidden' : ''}>
+        <Div100vh>
+          <div className="fixed px-10 py-8 top-0 right-0 w-screen h-screen z-[10]">
+            {isHidden ? null : (
+              <div
+                className="absolute bg-[rgba(0,0,0,0.2)] px-10 py-8 top-0 right-0  w-screen h-screen !overflow-y-scroll z-[-1]"
+                onClick={() => {
+                  setIsHidden(!isHidden)
+                }}
+              ></div>
+            )}
+            <button
+              className="bg-white w-full py-2 pr-1 items-center border-b flex justify-end text-2xl"
+              onClick={(e) => {
+                e.preventDefault()
                 setIsHidden(!isHidden)
               }}
-            ></div>
-          )}
-          <button
-            className="bg-white w-full py-2 pr-1 items-center border-b flex justify-end text-2xl"
-            onClick={(e) => {
-              e.preventDefault()
-              setIsHidden(!isHidden)
-            }}
-          >
-            <MdOutlineCancel />
-          </button>
-          <div className="bg-white h-[calc(90vh-114px)] overflow-y-scroll z-[10]">
-            <div className="mb-4">
-              <p className="px-4 py-2 border-b-2 font-bold">選擇行程(必選)</p>
-              {AttrCounts.map((item, index) => {
-                return (
-                  <label key={index} className="flex justify-between px-4 py-2">
-                    {item.name}
-                    <input
-                      type="radio"
-                      value={item.value}
-                      {...register('AttrCounts', { required: true })}
-                    />
-                  </label>
-                )
-              })}
-            </div>
-
-            <div className="mb-4">
-              <p className="px-4 py-2 border-y-2 font-bold">選擇交通工具</p>
-              {Transports.map((item, index) => {
-                return (
-                  <label key={index} className="flex justify-between px-4 py-2">
-                    {item.name}
-                    <input
-                      type="radio"
-                      value={item.value}
-                      {...register('Transports', { required: true })}
-                    />
-                  </label>
-                )
-              })}
-            </div>
-
-            <div className="mb-6">
-              <p className="px-4 py-2 border-y-2 font-bold">選擇區域(必選)</p>
-              <label className="flex justify-between px-4 py-2 border-b-2">
-                鄰近
-                <input
-                  type="checkbox"
-                  {...register('nearBy')}
-                  onClick={() => {
-                    setValue('DistrictName', [])
-                  }}
-                />
-              </label>
-              {DistrictName.map((item, index) => {
-                const handleOnChange = (
-                  e: React.ChangeEvent<HTMLInputElement>
-                ) => {
-                  const DistrictName = { ...register('DistrictName') }
-                  function setCurrentValue(bool: boolean) {
-                    DistrictName.onChange(e)
-                    const data = watch('DistrictName')
-                    setValue('nearBy', false)
-                    // 篩選data，設定表單
-                    setValue(
-                      'DistrictName',
-                      data.filter((item: string) => {
-                        return bool ? item !== '不限' : item === '不限'
-                      })
-                    )
-                  }
-                  // 判斷是否為'不限'，執行不同參數函式
-                  item !== '不限'
-                    ? setCurrentValue(true)
-                    : setCurrentValue(false)
-                }
-                return (
-                  <label key={index} className="flex justify-between px-4 py-2">
-                    {item}
-                    <input
-                      type="checkbox"
-                      value={item}
-                      {...register('DistrictName')}
-                      onChange={handleOnChange}
-                    />
-                  </label>
-                )
-              })}
-            </div>
-
-            {/* <hr /> */}
-          </div>
-          <div className="flex bg-white border-t justify-between px-4 py-4">
-            <button type="button" className="underline">
-              清除全部(預設值)
-            </button>
-            <button
-              form={formIdMobile}
-              className="bg-[#ccc] px-8 py-2"
-              onClick={handleErrors}
             >
-              送出
+              <MdOutlineCancel />
             </button>
+            <div className="bg-white h-[calc(90vh-114px)] overflow-y-scroll z-[10]">
+              <div className="mb-4">
+                <p className="px-4 py-2 border-b-2 font-bold">選擇行程(必選)</p>
+                {AttrCounts.map((item, index) => {
+                  return (
+                    <label
+                      key={index}
+                      className="flex justify-between px-4 py-2"
+                    >
+                      {item.name}
+                      <input
+                        type="radio"
+                        value={item.value}
+                        {...register('AttrCounts', { required: true })}
+                      />
+                    </label>
+                  )
+                })}
+              </div>
+
+              <div className="mb-4">
+                <p className="px-4 py-2 border-y-2 font-bold">選擇交通工具</p>
+                {Transports.map((item, index) => {
+                  return (
+                    <label
+                      key={index}
+                      className="flex justify-between px-4 py-2"
+                    >
+                      {item.name}
+                      <input
+                        type="radio"
+                        value={item.value}
+                        {...register('Transports', { required: true })}
+                      />
+                    </label>
+                  )
+                })}
+              </div>
+
+              <div className="mb-6">
+                <p className="px-4 py-2 border-y-2 font-bold">選擇區域(必選)</p>
+                <label className="flex justify-between px-4 py-2 border-b-2">
+                  鄰近
+                  <input
+                    type="checkbox"
+                    {...register('nearBy')}
+                    onClick={() => {
+                      setValue('DistrictName', [])
+                    }}
+                  />
+                </label>
+                {DistrictName.map((item, index) => {
+                  const handleOnChange = (
+                    e: React.ChangeEvent<HTMLInputElement>
+                  ) => {
+                    const DistrictName = { ...register('DistrictName') }
+                    function setCurrentValue(bool: boolean) {
+                      DistrictName.onChange(e)
+                      const data = watch('DistrictName')
+                      setValue('nearBy', false)
+                      // 篩選data，設定表單
+                      setValue(
+                        'DistrictName',
+                        data.filter((item: string) => {
+                          return bool ? item !== '不限' : item === '不限'
+                        })
+                      )
+                    }
+                    // 判斷是否為'不限'，執行不同參數函式
+                    item !== '不限'
+                      ? setCurrentValue(true)
+                      : setCurrentValue(false)
+                  }
+                  return (
+                    <label
+                      key={index}
+                      className="flex justify-between px-4 py-2"
+                    >
+                      {item}
+                      <input
+                        type="checkbox"
+                        value={item}
+                        {...register('DistrictName')}
+                        onChange={handleOnChange}
+                      />
+                    </label>
+                  )
+                })}
+              </div>
+
+              {/* <hr /> */}
+            </div>
+            <div className="flex bg-white border-t justify-between px-4 py-4">
+              <button type="button" className="underline">
+                清除全部(預設值)
+              </button>
+              <button
+                form={formIdMobile}
+                className="bg-[#ccc] px-8 py-2"
+                onClick={handleErrors}
+              >
+                送出
+              </button>
+            </div>
           </div>
-        </form>
-      </Div100vh>
+        </Div100vh>
+      </form>
     </>
   )
 }
