72363b95a5137a7efc539cac1e4919c525bc63e4
ChiaYang <aa235553756@gmail.com>
2023-04-28 12:09:28 +0800

    [fix]修改header搜尋欄以及footer版權文字

--- src/common/components/Footer.tsx
+++ src/common/components/Footer.tsx
@@ -4,7 +4,21 @@ export default function Footer() {
   return (
     <div className="text-gray-A8 text-center py-6 border-t-[1px] border-gray-E7 md:py-12 md:h-[120px]">
       <div className="container">
-       <p className='text-sm md:text-base'>COPYRIGHT © Travel Maker  ALL RIGHTS RESERVED.</p>
+        <p className="text-sm md:text-base">
+          COPYRIGHT © Travel Maker ALL RIGHTS RESERVED.
+        </p>
+        <br />
+        <span className="text-sm text-gray-D underline">
+          本網站上所有的圖片，如果您是版權擁有者，發現本網站上的任何圖片未經您的明確許可使用，請立即與我們聯繫，我們將盡快刪除該圖片或進行其他必要的更正措施。
+          <br />
+          <span>請郵寄至此 </span>
+          <a
+            href="mailto:traveltourgogo@gmail.com"
+            className="hover:text-primary-dark active:text-primary-tint pb-10 underline"
+          >
+            traveltourgogo@gmail.com
+          </a>
+        </span>
       </div>
     </div>
   )
