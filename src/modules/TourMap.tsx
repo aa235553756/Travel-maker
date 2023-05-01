abf0bc5ce6d492a463906c45e1316efb3084a047
ChiaYang <aa235553756@gmail.com>
2023-05-01 15:04:26 +0800

    [fix]修正房間頁地圖大小

--- src/modules/TourMap.tsx
+++ src/modules/TourMap.tsx
@@ -23,7 +23,7 @@ export default function TourMap({ data }: { data: RoomAttractionsProp[] }) {
       : ''
 
   return (
-    <div className="w-full h-full max-w-[840px] max-h-[312px] mb-12 bg-gray-D9  rounded-md">
+    <div className="w-full h-full max-w-[840px] max-h-[316px] mb-9 bg-gray-D9  rounded-md">
       <iframe
         src={`https://www.google.com/maps/embed/v1/directions?key=${
           process.env.NEXT_PUBLIC_YANG_GOOGLE_KEY
